d3d2665a19a235fafa912cbc6d25da5bb4584526
Fredy ID <56205156+fredy-ID@users.noreply.github.com>
2025-01-07 11:08:47 +0400

    Jour dev 2
    Suppression de toutes les sections inutiles. Modification de la section projet pour informer les recruteurs de ce que je fait

---
+++ index.js
@@ -0,0 +1,7 @@
+function openModal(id) {
+    document.getElementById(`${id}-modal`).style.display = 'flex';
+}
+
+function closeModal(id) {
+    document.getElementById(`${id}-modal`).style.display = 'none';
+}
